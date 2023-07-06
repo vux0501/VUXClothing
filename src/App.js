525778352c5ae6d13ccf883e37e3ecf67137e64e
vux0501 <hvudtkt@gmail.com>
2023-07-06 13:26:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import { DownloadOutlined } from '@ant-design/icons';
+import './App.scss';
+import { Button, Space } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button
+                    type="primary"
+                    shape="circle"
+                    icon={<DownloadOutlined />}
+                    size={36}
+                    className="containerbutton"
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
