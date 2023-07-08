2383292af171a78416c1a869bd68a113f66923e5
vux0501 <hvudtkt@gmail.com>
2023-07-08 15:44:25 +0700

    api register

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -2,9 +2,30 @@ const UserSevice = require('../services/UserService');
 
 const createUser = async (req, res) => {
     try {
-        console.log(req.body);
-        const res = await UserSevice.createUser();
-        return res.status(200).json();
+        const { name, email, password, confirmPassword, phone, avatar } = req.body;
+        const reg =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const isCheckEmail = reg.test(email);
+
+        if (!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required.',
+            });
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email.',
+            });
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal to the Confirm Password',
+            });
+        }
+
+        const response = await UserSevice.createUser(req.body);
+        return res.status(200).json(response);
     } catch (error) {
         return res.status(404).json({
             message: error,
--- server/src/services/UserService.js
+++ server/src/services/UserService.js
@@ -1,8 +1,46 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require('../models/UserModel');
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone, avatar } = newUser;
         try {
-            resolve({});
-        } catch (e) {}
+            const checkUserEmail = await User.findOne({
+                email: email,
+            });
+            const checkUserPhone = await User.findOne({
+                phone: phone,
+            });
+
+            if (checkUserEmail !== null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The email is already',
+                });
+            } else if (checkUserPhone !== null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The phone is already',
+                });
+            } else {
+                const createdUser = await User.create({
+                    name: name,
+                    email: email,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                    phone: phone,
+                    avatar: avatar,
+                });
+                if (createdUser) {
+                    resolve({
+                        status: 'OK',
+                        message: 'Create new user successfully',
+                        data: createdUser,
+                    });
+                }
+            }
+        } catch (e) {
+            reject({});
+        }
     });
 };
 
