68e3dc17040eaab7b21a1bc190a8174d627898d9
vux0501 <hvudtkt@gmail.com>
2023-07-12 19:50:05 +0700

    install react query

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import Layout from './Layout';
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Layout />
-        </BrowserRouter>
-    </Provider>,
+    <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Layout />
+            </BrowserRouter>
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
